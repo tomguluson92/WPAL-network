e6ef07fbdfab4cb2dd5d8d750212a57acd8030af
YangZhou1994 <zy295479271@163.com>
2017-04-07 14:56:58 +0800

    Debug

--- lib/utils/rap_db.py
+++ lib/utils/rap_db.py
@@ -91,7 +91,8 @@ class RAP:
             self.img_tmp = cv2.imread(self.path_tmp)
             self.height, self.width = self.img_tmp.shape[:2]
             self.size_ratio_tmp = round(float(self.height) / float(self.width))
-            print i
+            if i%1000 == 0:
+                print i
             if self.size_ratio_tmp > 10:
                self.size_ratio_tmp = 10
             self.train_classified_pre[int(self.size_ratio_tmp)].append(i)
