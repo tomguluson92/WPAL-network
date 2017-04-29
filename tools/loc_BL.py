243c1595e41e77bfb83e2b68296288e248bffb60
YangZhou1994 <zy295479271@163.com>
2017-04-29 20:37:25 +0800

    Debug

--- tools/loc_BL.py
+++ tools/loc_BL.py
@@ -102,10 +102,10 @@ if __name__ == '__main__':
     print('Called with args:')
     print(args)
 
-    if args.cfg_file is not None:
-        cfg_from_file(args.cfg_file)
-    if args.set_cfgs is not None:
-        cfg_from_list(args.set_cfgs)
+ #   if args.cfg_file is not None:
+  #      cfg_from_file(args.cfg_file)
+  #  if args.set_cfgs is not None:
+  #      cfg_from_list(args.set_cfgs)
 
     cfg.GPU_ID = args.gpu_id
 
